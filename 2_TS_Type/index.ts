710b9815550295a11a25beee96c2538d64d1e883
TakeuchiKyohei <128.takeuchi.kyohei@gmail.com>
2026-02-09 00:21:46 +0900

    ts_type リストの型定義(タプル)

--- 2_TS_Type/index.ts
+++ 2_TS_Type/index.ts
@@ -13,3 +13,5 @@ const person = {
 const fruits = ['apple', 'banana',1] // 型推論
 const fruits2: string[] = ['apple', 'banana'] // 型指定
 const fruits3: (string|number)[] = ['apple', 'banana'] // 型指定 union型
+
+const book: [string, number, boolean] = ['business', 1500, false] // リストの型をタプルで指定
