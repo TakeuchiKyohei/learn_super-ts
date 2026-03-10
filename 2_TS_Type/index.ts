4d2eb2695783b334a0880f7f87b4ff634cdcaf12
TakeuchiKyohei <128.takeuchi.kyohei@gmail.com>
2026-03-11 00:48:03 +0900

    typeエイリアス

--- 2_TS_Type/index.ts
+++ 2_TS_Type/index.ts
@@ -39,6 +39,7 @@ unionType = 'hello';
 unionType.toUpperCase // strが入っている時はstrのメソッドが
 let unionType2: (number | string)[] = [1, 'hello']
 
+type ClothSize = 's' | 'm' | 'l' 
 const apple: 'apple' = 'apple'
 const apple2 = 'apple' // constで描き始めたら定義を省略できる
-let clothSize: 's' | 'm' | 'l' = 'l' //literal + unionでenumのような制限をかけることができる
+let clothSize: ClothSize = 'l' //literal + unionでenumのような制限をかけることができる
