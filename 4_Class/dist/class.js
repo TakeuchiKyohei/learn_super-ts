f8546d43c425ca11ad76dec98096a22ae3a5c5be
TakeuchiKyohei <128.takeuchi.kyohei@gmail.com>
2026-03-21 20:29:42 +0900

    class private/pubricの使い方

--- 4_Class/dist/class.js
+++ 4_Class/dist/class.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Person {
-    constructor(initName) {
+    constructor(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
+    }
+    incrementAge() {
+        this.age++;
     }
     greet() {
-        console.log(`Hello, I'm ${this.name}.`);
+        console.log(`Hello, I'm ${this.name} and I'm ${this.age} years old.`);
     }
 }
-const taro = new Person("Taro");
+const taro = new Person("Taro", 20);
+// taro.age = 42 // privateにすることでアクセスできなくなる
 taro.greet();
-const anotherTaro = {
-    name: "Taro",
-    anotherGreet: taro.greet
-};
-anotherTaro.anotherGreet();
--- 4_Class/src/class.ts
+++ 4_Class/src/class.ts
@@ -1,18 +1,18 @@
 class Person {
   name: string;
-  constructor(initName: string) {
+  private age: number;
+  constructor(initName: string, initAge: number) {
     this.name = initName;
+    this.age = initAge;
+  }
+  incrementAge() {
+    this.age++;
   }
   greet(this: Person) { // thisの型を指定
-        console.log(`Hello, I'm ${this.name}.`);
+        console.log(`Hello, I'm ${this.name} and I'm ${this.age} years old.`);
     }
 }
 
-const taro = new Person("Taro");
+const taro = new Person("Taro", 20);
+// taro.age = 42 // privateにすることでアクセスできなくなる
 taro.greet();
-
-const anotherTaro = {
-  name: "Taro",
-  greet: taro.greet
-}
-anotherTaro.greet();
