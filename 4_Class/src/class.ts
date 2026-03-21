0db3bc5cf303d3daa710ba2235ea33387b2a9b49
TakeuchiKyohei <128.takeuchi.kyohei@gmail.com>
2026-03-21 19:58:22 +0900

    class thisの挙動を正しく使うために

--- 4_Class/src/class.ts
+++ 4_Class/src/class.ts
@@ -3,7 +3,16 @@ class Person {
   constructor(initName: string) {
     this.name = initName;
   }
+  greet(this: { name: string }) { // thisの型を指定
+        console.log(`Hello, I'm ${this.name}.`);
+    }
 }
 
 const taro = new Person("Taro");
-console.log(taro);
+taro.greet();
+
+const anotherTaro = {
+  name: "Taro",
+  anotherGreet: taro.greet
+}
+anotherTaro.anotherGreet();
