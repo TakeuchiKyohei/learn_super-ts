b07579a1c2789df45fb281d042745989a65d578a
TakeuchiKyohei <128.takeuchi.kyohei@gmail.com>
2026-03-23 22:11:45 +0900

    interface メソッドの型の書き方

--- 5_Interface/dist/interface.js
+++ 5_Interface/dist/interface.js
@@ -3,4 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const human = {
     name: "Taro",
     age: 30,
+    greet(message) {
+        console.log(`${message}, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
 };
--- 5_Interface/src/interface.ts
+++ 5_Interface/src/interface.ts
@@ -1,9 +1,14 @@
 interface Human {
   name: string;
   age: number;
+  // greet: (message: string) => void; この書き方もできる
+  greet(message: string): void;
 }
 
 const human: Human = {
   name: "Taro",
   age: 30,
+  greet(message: string): void {
+    console.log(`${message}, I'm ${this.name} and I'm ${this.age} years old.`);
+  }
 }
