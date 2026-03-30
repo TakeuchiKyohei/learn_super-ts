32b1068b9c205fbd33c5c1786711b4bc72f05102
TakeuchiKyohei <128.takeuchi.kyohei@gmail.com>
2026-03-30 20:59:27 +0900

    インターセクション型について

---
+++ 6_Advanced/advanced.js
@@ -0,0 +1,10 @@
+var quill = {
+    name: "Quill",
+    role: "Frontend Engineer",
+    follower: 1000,
+};
+var quill2 = {
+    name: "Quill",
+    role: "Frontend Engineer",
+    follower: 1000,
+};
