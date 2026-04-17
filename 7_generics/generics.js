41d6dfb677428ac19205a236b4ffb4d1f68db7ae
TakeuchiKyohei <128.takeuchi.kyohei@gmail.com>
2026-04-17 22:36:31 +0900

    generics classに対して使ってみる

--- 7_generics/generics.js
+++ 7_generics/generics.js
@@ -3,4 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function copy(value, key) {
     return value;
 }
-console.log(copy({ name: "Taro" }, "name"));
+console.log(copy({ name: "Taro", age: 30 }, "age"));
+class LightDatabase {
+    constructor() {
+        this.data = [];
+    }
+    add(item) {
+        this.data.push(item);
+    }
+    remove(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringDatabase = new LightDatabase();
+stringDatabase.add('Apple');
+console.log(stringDatabase.get());
--- 7_generics/generics.ts
+++ 7_generics/generics.ts
@@ -1,4 +1,20 @@
 function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T {
   return value;
 }
-console.log(copy({name: "Taro"}, "name"));
+console.log(copy({name: "Taro", age: 30}, "age"));
+
+class LightDatabase<T extends string | number | boolean> {
+  private data: T[] = [];
+  add(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  get(){
+    return this.data;
+  }
+}
+const stringDatabase = new LightDatabase<string>();
+stringDatabase.add('Apple');
+console.log(stringDatabase.get());
